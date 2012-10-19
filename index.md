431cdfc15e76811ec6889242084ffc1daa9b0579
Marchenko Alexandr <marchenko.alexandr@gmail.com>
2012-10-19 10:42:20 +0300

    some redesign

--- index.md
+++ index.md
@@ -3,13 +3,15 @@ layout: base
 title: My notes
 ---
 
-<article class="recent">
+<aside class="recent">
 <h1>Recent</h1>
 <ul>
 {% for post in site.posts limit:20 %}
     <li><a href="{{ post.url }}">{{ post.title }}</a></li>
 {% endfor %}
 </ul>
+</aside>
+<aside class="howto">
 <h1>How to</h1>
 <ul>
 {% for page in site.pages %}
@@ -18,8 +20,15 @@ title: My notes
     {% endif %}
 {% endfor %}
 </ul>
-</article>
-<aside class="pipe">
-<h1>Pipe</h1>
-<ul id="pipe"></ul>
 </aside>
+<div id="cse" style="width: 100%;clear:both;">Loading</div>
+<script src="//www.google.com/jsapi"> </script>
+<script>
+    google.load('search', '1');
+    google.setOnLoadCallback(function() {
+        var customSearchOptions = {};
+        var customSearchControl = new google.search.CustomSearchControl('000857461493106615220:h3g48vufhro', customSearchOptions);
+        customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+        customSearchControl.draw('cse');
+    }, true);
+</script>
--- js/scripts.js
+++ js/scripts.js
@@ -47,29 +47,6 @@ if(typeof prettyPrint != 'undefined') {
     prettyPrint();
 }
 
-function mypipe(data) {
-    var ul = document.getElementById('pipe');
-    for(var i = 0; i < data.value.items.length; i++) {
-        var item = data.value.items[i];
-        var a = document.createElement('A');
-        var li = document.createElement('LI');
-
-        a.href = item.link;
-        a.innerHTML = item.title;
-
-        li.appendChild(a);
-        ul.appendChild(li);
-    }
-}
-
-// if(document.getElementById('disqus_thread') && window.location.hostname != 'localhost') {
-//     var disqus_shortname = 'mbou';
-// 
-//     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-//     dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-//     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-// }
-
 function toggleImgNoScale(e) {
     this.className = this.className == 'noscale' ? '' : 'noscale';
 }
